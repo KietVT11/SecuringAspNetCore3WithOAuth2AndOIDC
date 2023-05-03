1a2197e404d7ade6af532d05995698f373593cad
Kiet Vo <kietvt.dlu@gmail.com>
2023-05-04 00:14:10 +0700

    16 Demo - Logging out of Our Web Application

---
+++ Starter files/src/ImageGallery.Client/Controllers/AuthenticationController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImageGallery.Client.Controllers
+{
+	public class AuthenticationController : Controller
+	{
+		[Authorize]
+		public async Task Logout()
+		{
+			// Clears the local cookie
+			await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+		}
+	}
+}
