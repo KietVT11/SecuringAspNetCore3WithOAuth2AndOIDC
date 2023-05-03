240bc3cd0d31268a7467db2c49641fecb27e94a6
Kiet Vo <kietvt.dlu@gmail.com>
2023-05-04 00:40:46 +0700

    17 Demo - Redirecting After Logging out

--- Starter files/src/ImageGallery.Client/Controllers/AuthenticationController.cs
+++ Starter files/src/ImageGallery.Client/Controllers/AuthenticationController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,6 +13,12 @@ namespace ImageGallery.Client.Controllers
 		{
 			// Clears the local cookie
 			await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+			// Redirects to the IDP linked to scheme
+			// "OpenIdConnectDefaults.AuthenticationScheme" (oidc)
+			// so it can clear its own session/cookie
+			await HttpContext.SignOutAsync(
+				OpenIdConnectDefaults.AuthenticationScheme);
 		}
 	}
 }
